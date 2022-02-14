d972665c623299794328f914f055c1926e2b8f54
Artyom Danilov <defytheflow@gmail.com>
2022-02-14 23:14:10 +0300

    feat: display ingredients on ingredients page

--- cocktails/ContentView.swift
+++ cocktails/ContentView.swift
@@ -7,98 +7,121 @@
 
 import SwiftUI
 
+let THE_WIDTH = CGFloat(390)
+
 struct ContentView: View {
   @EnvironmentObject var network: Network
   
   var body: some View {
     NavigationView {
-    
-    ZStack {
-      VStack(spacing: -25) {
-        if let cocktail = network.cocktail {
-          AsyncImage(url: URL(string: cocktail.strDrinkThumb)) { image in
-            image.resizable()
-          } placeholder: {
-            ProgressView()
-          }
-          .frame(width: 390, height: 390)
-          
-          VStack {
-            Text(cocktail.strDrink)
-              .bold()
-              .frame(maxWidth: .infinity, alignment: .leading)
-              .padding()
-              .padding(.top, 10)
-              .padding(.horizontal, 5)
-              .font(.system(size: 32))
-            
-            RoundedRectangle(cornerRadius: 100)
-              .frame(width: 350, height: 5)
-              .foregroundColor(Color.ui.primary)
-            
-            Text(cocktail.strCategory)
-              .frame(maxWidth: .infinity, alignment: .leading)
-              .padding()
-              .padding(.horizontal, 5)
-              .font(.system(size: 22))
-            
-            Text(cocktail.strAlcoholic)
-              .frame(maxWidth: .infinity, alignment: .leading)
-              .padding()
-              .padding(.horizontal, 5)
-              .font(.system(size: 22))
-            
-            Spacer()
+      ZStack {
+        VStack(spacing: -25) {
+          if let cocktail = network.cocktail {
+            AsyncImage(url: URL(string: cocktail.strDrinkThumb)) { image in
+              image.resizable()
+            } placeholder: {
+              ProgressView()
+            }
+            .frame(width: THE_WIDTH, height: 390)
             
             VStack {
-              NavigationLink(destination: IngredientsView()) {
-                Text("Show ingredients")
-                  .foregroundColor(Color.ui.primary)
+              Heading(children: cocktail.strDrink)
+              
+              RoundedRectangle(cornerRadius: 100)
+                .frame(width: 350, height: 5)
+                .foregroundColor(Color.ui.primary)
+              
+              Text(cocktail.strCategory)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.horizontal, 5)
+                .font(.system(size: 22))
+              
+              Text(cocktail.strAlcoholic)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+                .padding(.horizontal, 5)
+                .font(.system(size: 22))
+              
+              Spacer()
+              
+              VStack {
+                NavigationLink(destination: IngredientsView(cocktail: cocktail)) {
+                  Text("Show ingredients")
+                    .foregroundColor(Color.ui.primary)
+                }
+                Button(action: network.fetchCocktail) {
+                  Text("Next cocktail")
+                }
+                .padding(.horizontal, 30)
+                .padding(.vertical, 15)
+                .foregroundColor(Color.ui.background)
+                .background(Color.ui.primary)
+                .clipShape(Capsule())
               }
-              Button(action: network.fetchCocktail) {
-                Text("Next cocktail")
-              }
-              .padding(.horizontal, 30)
-              .padding(.vertical, 15)
-              .foregroundColor(Color.ui.background)
-              .background(Color.ui.primary)
-              .clipShape(Capsule())
+              .font(.system(size: 25))
+              .padding(.bottom, 20)
             }
-            .font(.system(size: 25))
-            .padding(.bottom, 20)
+            
+            .background(Color.ui.background)
+            .clipShape(RoundedRectangle(cornerRadius: 20.0))
           }
-          
-          .background(Color.ui.background)
-          .clipShape(RoundedRectangle(cornerRadius: 20.0))
+          Spacer()
         }
-        Spacer()
-      }
-      
-      VStack{
-        Button(action: {}) {
-          Text("+")
-        }
-        .padding(25)
-        .foregroundColor(.white)
-        .background(Color.ui.primary)
-        .clipShape(Circle())
-        .font(.system(size: 30))
-        .frame(maxWidth: 325, alignment: .trailing)
         
-        Spacer().frame(height: 50)
+        VStack {
+          Button(action: {}) {
+            Text("+")
+          }
+          .padding(25)
+          .foregroundColor(.white)
+          .background(Color.ui.primary)
+          .clipShape(Circle())
+          .font(.system(size: 30))
+          .frame(maxWidth: 325, alignment: .trailing)
+          
+          Spacer().frame(height: 50)
+        }
       }
+      .navigationBarHidden(true)
     }
-    }
-    
     .onAppear(perform: network.fetchCocktail)
   }
-  
+}
+
+struct Heading: View {
+  var children: String
+  var body: some View {
+      Text(children)
+        .bold()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+        .padding(.top, 10)
+        .padding(.horizontal, 5)
+        .font(.system(size: 32))
+  }
 }
 
 
 struct IngredientsView: View {
+  var cocktail: Cocktail
+  
   var body: some View {
-    Text("Ingredients view")
+    VStack {
+      Heading(children: cocktail.strDrink)
+      Text(cocktail.strInstructions)
+      ScrollView {
+        ForEach(cocktail.getIngredients()) {
+          AsyncImage(url: URL(string: $0.getImageURL())) { image in
+            image.resizable()
+          } placeholder: {
+            ProgressView()
+          }
+          .frame(width: 50, height: 50)
+          Text("\($0.name) - \($0.measure)")
+        }
+      }
+    }
   }
 }
 
@@ -110,16 +133,6 @@ struct ContentView_Previews: PreviewProvider {
   }
 }
 
-//class MyClass {
-//  var myProperty: String
-//
-//  init(myProperty: String) {
-//    self.myProperty = myProperty
-//  }
-//}
-//
-//MyClass(myProperty: "hello")
-
-func myFunc(myAttr: String) {
-    
+func test() {
+  //
 }
--- cocktails/Network.swift
+++ cocktails/Network.swift
@@ -8,64 +8,114 @@
 import SwiftUI
 
 class Network: ObservableObject {
-    @Published var cocktail: Cocktail? = nil
+  @Published var cocktail: Cocktail? = nil
 
-    func fetchCocktail() {
-        guard let url = URL(string: "https://www.thecocktaildb.com/api/json/v1/1/random.php") else { fatalError("Missing URL") }
-        let urlRequest = URLRequest(url: url)
+  func fetchCocktail() {
+    guard let url = URL(string: "https://www.thecocktaildb.com/api/json/v1/1/random.php") else { fatalError("Missing URL") }
+    let urlRequest = URLRequest(url: url)
 
-        let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            if let error = error {
-                print("Request error:", error)
-                return
-            }
+    let dataTask = URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+      if let error = error {
+        print("Request error:", error)
+        return
+      }
 
-            guard let response = response as? HTTPURLResponse else { return }
+      guard let response = response as? HTTPURLResponse else { return }
 
-            if response.statusCode == 200 {
-                guard let data = data else { return }
-                DispatchQueue.main.async {
-                    do {
-                        let decoder = JSONDecoder()
-                        let cocktailData = try decoder.decode(CocktailData.self, from: data)
-                        self.cocktail = cocktailData.drinks[0]
-                        print("Fetched cocktail", self.cocktail)
-                    } catch let error {
-                        print("Error decoding:", error)
-                    }
-                }
-            }
+      if response.statusCode == 200 {
+        guard let data = data else { return }
+        DispatchQueue.main.async {
+          do {
+            let decoder = JSONDecoder()
+            let cocktailData = try decoder.decode(CocktailData.self, from: data)
+            self.cocktail = cocktailData.drinks[0]
+            print("Fetched cocktail", self.cocktail)
+          } catch let error {
+            print("Error decoding:", error)
+          }
         }
-
-        dataTask.resume()
+      }
     }
+
+    dataTask.resume()
+  }
 }
 
 struct CocktailData: Decodable {
-    var drinks: [Cocktail]
+  let drinks: [Cocktail]
 }
 
 struct Cocktail: Decodable {
-    var idDrink: String
-    var strDrink: String
-    var strCategory: String
-    var strAlcoholic: String
-    var strGlass: String
-    var strInstructions: String
-    var strDrinkThumb: String
-    var strIngredient1: String?
-    var strIngredient2: String?
-    var strIngredient3: String?
-    var strIngredient4: String?
-    var strIngredient5: String?
-    var strIngredient6: String?
-    var strIngredient7: String?
-    var strIngredient8: String?
-    var strIngredient9: String?
-    var strIngredient10: String?
-    var strIngredient11: String?
-    var strIngredient12: String?
-    var strIngredient13: String?
-    var strIngredient14: String?
-    var strIngredient15: String?
+  let idDrink: String
+  let strDrink: String
+  let strCategory: String
+  let strAlcoholic: String
+  let strGlass: String
+  let strInstructions: String
+  let strDrinkThumb: String
+
+  let strIngredient1: String?
+  let strIngredient2: String?
+  let strIngredient3: String?
+  let strIngredient4: String?
+  let strIngredient5: String?
+  let strIngredient6: String?
+  let strIngredient7: String?
+  let strIngredient8: String?
+  let strIngredient9: String?
+  let strIngredient10: String?
+  let strIngredient11: String?
+  let strIngredient12: String?
+  let strIngredient13: String?
+  let strIngredient14: String?
+  let strIngredient15: String?
+
+  let strMeasure1: String?
+  let strMeasure2: String?
+  let strMeasure3: String?
+  let strMeasure4: String?
+  let strMeasure5: String?
+  let strMeasure6: String?
+  let strMeasure7: String?
+  let strMeasure8: String?
+  let strMeasure9: String?
+  let strMeasure10: String?
+  let strMeasure11: String?
+  let strMeasure12: String?
+  let strMeasure13: String?
+  let strMeasure14: String?
+  let strMeasure15: String?
+
+  func getIngredients() -> [Ingredient] {
+    // This is pure masterpiece.
+    var ingredients: [Ingredient] = []
+
+    if let n = strIngredient1,  let m =  strMeasure1 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient2,  let m =  strMeasure2 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient3,  let m =  strMeasure3 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient4,  let m =  strMeasure4 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient5,  let m =  strMeasure5 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient6,  let m =  strMeasure6 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient7,  let m =  strMeasure7 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient8,  let m =  strMeasure8 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient9,  let m =  strMeasure9 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient10, let m = strMeasure10 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient11, let m = strMeasure11 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient12, let m = strMeasure12 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient13, let m = strMeasure13 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient14, let m = strMeasure14 { ingredients.append(Ingredient(name: n, measure: m)) }
+    if let n = strIngredient15, let m = strMeasure15 { ingredients.append(Ingredient(name: n, measure: m)) }
+
+    return ingredients
+  }
+}
+
+struct Ingredient: Identifiable {
+  let name: String
+  let measure: String
+  let id = UUID()
+  
+  func getImageURL() -> String {
+    return "https://www.thecocktaildb.com/images/ingredients/\(name)-Small.png".addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+  }
 }
